3d00a3e9a4e9942858c3f933f5570758caf3c3b5
Anonymous Applicant <anonymous@example.com>
2018-05-16 16:08:40 -0400

    working API invocation with hardcoded ocd-ids

--- src/my_exercise/search.clj
+++ src/my_exercise/search.clj
@@ -1,21 +1,36 @@
 (ns my-exercise.search
   "Voter Search request and results"
-  (:require  [ring.util.response :as resp]
-             [hiccup.page :refer [html5]]
-             [my-exercise.home :as home]))
+  (:require [clojure.string :as str]
+            [clj-http.client :as client]
+            [ring.util.response :as resp]
+            [hiccup.page :refer [html5]]
+            [my-exercise.home :as home]
+))
 
-(defn search-results
+;; In a real app, would pull this out to a config file or settings DB
+;; instead of a hardcoded map
+(def api-urls {:upcoming "https://api.turbovote.org/elections/upcoming"})
+
+(defn- search-results
   "Server-side rendering of the search results page"
-  [request]
+  [results]
   (html5
     ;; reuse the home header. Should probably factor out to common lib
     ;; code (if always the same) or create a separate header (if
     ;; always different.)
-    (home/header request)
+    (home/header nil)
     [:div "Hello, world!"]))
 
 (defn search
-  "Ring handler for voter search POST"
+  "Ring handler for upcoming election POST request"
   [request]
-  (search-results request))
+  ;; Note: we'll use synchronous HTTP here because its
+  ;; simpler. If we needed to coordinate the results of muptiple
+  ;; backend services I would do so concurrently using
+  ;; core.async (or threads, or promises, or something like that.)
+  (let [ocd-ids ["ocd-division/country:us/state:nj"
+                 "ocd-division/country:us/state:nj/place:newark"]
+        results (client/get (:upcoming api-urls)
+                  {:query-params {:district-divisions (str/join "," ocd-ids)}})]
+    (search-results results)))
 
