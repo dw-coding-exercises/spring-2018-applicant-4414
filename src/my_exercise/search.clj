75ddc82179daa327247b3bf30a179ba3a1ddb6be
Anonymous Applicant <anonymous@example.com>
2018-05-16 17:12:37 -0400

    basic display of results

--- src/my_exercise/search.clj
+++ src/my_exercise/search.clj
@@ -13,15 +13,36 @@
 ;; instead of a hardcoded map
 (def api-urls {:upcoming "https://api.turbovote.org/elections/upcoming"})
 
+(defn- election
+  "Return the Hiccup markdown for an individual election"
+  [{:keys [description
+           polling-place-url
+           website]}]
+  [:div.election
+   [:h4 description]
+   [:ul
+    [:li [:a {:href website} "Voter Information"]]
+    [:li [:a {:href polling-place-url} "Polling Location"]]]
+   ;; TOOD: Add registration and polling instructions from the
+   ;; returned data. Just ran out of time here.
+   ])
+
+;; TODO: Add CSS to make this pretty
 (defn- search-results
   "Server-side rendering of the search results page"
-  [results]
+  [elections]
   (html5
     ;; reuse the home header. Should probably factor out to common lib
     ;; code (if always the same) or create a separate header (if
     ;; always different.)
     (home/header nil)
-    [:div "Hello, world!"]))
+    [:h1 "Election Search Results"]
+    (if (empty? elections)
+      [:p "No upcoming elections were found for the given address."]
+      [:div
+       [:p "We found the following upcoming elections for the given address:"]
+       [:div.elections
+        (map election elections)]])))
 
 
 (defn search
@@ -31,7 +52,7 @@
   ;; needed to coordinate the results of muptiple backend services I
   ;; would do so concurrently using core.async (or some other
   ;; coordination tool (promises or whatever).
-  (let [ocd-ids (ocd-ids/by-address (select-keys (:params request)
+  (let [ocd-ids (ocd-ids/for-address (select-keys (:params request)
                                       [:city :state :zip]))
         api-response (client/get (:upcoming api-urls)
                        {:query-params {:district-divisions (str/join "," ocd-ids)}})
